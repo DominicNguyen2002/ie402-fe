f008547d3926d257b9bb5ef0a4f56e03231129c6
Dominic-Nguyen <dominic.nguyen.hoang@gmail.com>
2024-05-30 08:59:08 +0700

    feat: basemap VN

--- src/components/ui/map.jsx
+++ src/components/ui/map.jsx
@@ -1,9 +1,54 @@
-import Map from '@arcgis/core/Map.js';
+import { loadModules } from '@arcgis/core/Map.js';
+import { useRef, useEffect } from 'react';
 
 export default function XMap() {
+  const mapRef = useRef(null);
+  useEffect(() => {
+    let view;
+
+    const initializeMap = async () => {
+      const [Map, MapView, GeoJSONLayer] = await loadModules([
+        'esri/Map',
+        'esri/views/MapView',
+        'esri/layers/GeoJSONLayer'
+      ]);
+
+      const map = new Map({
+        basemap: 'topo-vector'
+      });
+
+      view = new MapView({
+        container: mapRef.current,
+        map: map,
+        center: [0, 0], // Longitude, latitude
+        zoom: 2
+      });
+
+      const geojsonUrl = './basemap/vn-3.json';
+
+      const geojsonLayer = new GeoJSONLayer({
+        url: geojsonUrl,
+        popupTemplate: {
+          title: '{name}', // Replace "name" with an attribute in your data
+          content: '{description}' // Replace "description" with an attribute in your data
+        }
+      });
+
+      map.add(geojsonLayer);
+    };
+
+    initializeMap();
+
+    return () => {
+      if (view) {
+        view.destroy();
+      }
+    };
+  }, []);
+
   return (
-    <div className='w-80 h-80'>
-      <Map basemap='topo-vector'></Map>
+    <div className='w-full h-[100vh]' ref={mapRef}>
+      {/* <Map basemap='topo-vector'></Map> */}
     </div>
   );
 }
